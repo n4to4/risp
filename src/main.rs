07de3be29ca27dbf913c18af1218c3d8668eb294
n4to4 <n4to4k@gmail.com>
2021-07-15 17:51:54 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::collections::HashMap;
+
+#[derive(Clone)]
+enum RispExp {
+    Symbol(String),
+    Number(f64),
+    List(Vec<RispExp>),
+}
+
+#[derive(Debug)]
+enum RispErr {
+    Reason(String),
+}
+
+#[derive(Clone)]
+struct RispEnv {
+    data: HashMap<String, RispExp>,
+}
+
+fn main() {}
